e4070d59d475d4e7c6b20e965dbd9ca3dce5d730
Ivan Platonov <EventGraphDev@gmail.com>
2023-07-30 04:27:39 +0300

    Add project files.

---
+++ HelloConfig/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\";
+builder.WebHost.UseConfiguration(new HelloConfig.Settings(path).config);
+
+var app = builder.Build();
+IConfiguration configuration = app.Configuration;
+
+string city = $"Город: {configuration.GetSection("Country")["City"]}<br>";
+string html = $"<!DOCTYPE html><html><head><meta charset=utf-8></head><body><p style='color:{configuration.GetSection("Country")["Status"]}'>{city}</p></body></html>";
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync(html);
+});
+
+app.Run();
---
+++ HelloConfig/Settings.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloConfig
+{
+    public class Settings
+    {
+        public IConfiguration config { get; set; }
+        public Settings() 
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddInMemoryCollection(new Dictionary<string, string> 
+            { { "Country", "Россия" }, { "City", "Москва" } });
+
+            config = configurationBuilder.Build();
+        }
+        public Settings(string rootPath)
+        {
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.SetBasePath(rootPath);
+            configurationBuilder.AddJsonFile("appsettings.json");
+
+            config = configurationBuilder.Build();
+        }
+    }
+}
